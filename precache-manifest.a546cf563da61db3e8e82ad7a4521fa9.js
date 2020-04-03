dd822e0d674ff66f13ae8fee072e66887e825390
Shawn Harrison <38034484+theshawning@users.noreply.github.com>
2020-04-03 11:48:17 +0100

    Updates

---
+++ precache-manifest.a546cf563da61db3e8e82ad7a4521fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de33f99319112b8e0e30ed4771297e84",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2adc9f6c499fcd46c727",
+    "url": "/monsters-rolodex/static/css/main.69861762.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2adc9f6c499fcd46c727",
+    "url": "/monsters-rolodex/static/js/main.8c506a9f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
